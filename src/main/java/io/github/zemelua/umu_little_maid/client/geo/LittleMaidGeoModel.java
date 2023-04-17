58977461610251f36a23e66b9df0002b3acd8c77
Zemelua <zemelua1107@gmail.com>
2023-04-17 10:10:58 +0900

    🐸 メイドさんの頭が向いている方向に向くように

--- src/main/java/io/github/zemelua/umu_little_maid/client/geo/LittleMaidGeoModel.java
+++ src/main/java/io/github/zemelua/umu_little_maid/client/geo/LittleMaidGeoModel.java
@@ -2,10 +2,16 @@ package io.github.zemelua.umu_little_maid.client.geo;
 
 import io.github.zemelua.umu_little_maid.UMULittleMaid;
 import io.github.zemelua.umu_little_maid.entity.maid.LittleMaidEntity;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.util.Identifier;
+import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
+import software.bernie.geckolib3.core.manager.AnimationData;
+import software.bernie.geckolib3.core.processor.IBone;
 import software.bernie.geckolib3.model.AnimatedGeoModel;
+import software.bernie.geckolib3.model.provider.data.EntityModelData;
 
 public class LittleMaidGeoModel extends AnimatedGeoModel<LittleMaidEntity> {
+	static final String KEY_HEAD = "head";
 	static final String KEY_ARMOR_HEAD = "armorHead";
 	static final String KEY_ARMOR_LEFT_LEG = "armorLeftLeg";
 	static final String KEY_ARMOR_RIGHT_LEG = "armorRightLeg";
@@ -15,6 +21,24 @@ public class LittleMaidGeoModel extends AnimatedGeoModel<LittleMaidEntity> {
 	private static final Identifier MODEL_RESOURCE = UMULittleMaid.identifier("geo/little_maid.geo.json");
 	private static final Identifier TEXTURE_RESOURCE = UMULittleMaid.identifier("textures/entity/little_maid/little_maid.png");
 	private static final Identifier ANIMATION_RESOURCE = UMULittleMaid.identifier("animations/little_maid.animation.json");
+
+	@Override
+	@SuppressWarnings("unchecked")
+	public void setCustomAnimations(LittleMaidEntity maid, int instanceId, AnimationEvent animationEvent) {
+		super.setCustomAnimations(maid, instanceId, animationEvent);
+
+		IBone head = this.getBone(KEY_HEAD);
+
+		EntityModelData modelData = (EntityModelData) animationEvent.getExtraDataOfType(EntityModelData.class).get(0);
+
+		AnimationData manager = maid.getFactory().getOrCreateAnimationData(instanceId);
+		int unpause = !MinecraftClient.getInstance().isPaused() || manager.shouldPlayWhilePaused ? 1 : 0;
+
+		if (head != null) {
+			head.setRotationX(head.getRotationX() + (float) Math.toRadians(modelData.headPitch) * unpause);
+			head.setRotationY(head.getRotationY() + (float) Math.toRadians(modelData.netHeadYaw) * unpause);
+		}
+	}
 
 	@Override
 	public Identifier getModelResource(LittleMaidEntity object) {
