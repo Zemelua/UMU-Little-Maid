9be825ffcb674f50b5fa068d8d2213a5811c1df0
Zemelua <zemelua1107@gmail.com>
2023-04-28 17:27:00 +0900

    テクスチャを整理

--- src/main/java/io/github/zemelua/umu_little_maid/client/geo/LittleMaidGeoModel.java
+++ src/main/java/io/github/zemelua/umu_little_maid/client/geo/LittleMaidGeoModel.java
@@ -1,6 +1,7 @@
 package io.github.zemelua.umu_little_maid.client.geo;
 
 import io.github.zemelua.umu_little_maid.UMULittleMaid;
+import io.github.zemelua.umu_little_maid.entity.ModEntities;
 import io.github.zemelua.umu_little_maid.entity.maid.LittleMaidEntity;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.util.Identifier;
@@ -10,6 +11,7 @@ import software.bernie.geckolib3.core.processor.IBone;
 import software.bernie.geckolib3.model.AnimatedGeoModel;
 import software.bernie.geckolib3.model.provider.data.EntityModelData;
 
+@SuppressWarnings("unused")
 public class LittleMaidGeoModel extends AnimatedGeoModel<LittleMaidEntity> {
 	static final String KEY_HEAD = "head";
 	static final String KEY_LEFT_ARM = "left_arm";
@@ -21,11 +23,27 @@ public class LittleMaidGeoModel extends AnimatedGeoModel<LittleMaidEntity> {
 	static final String KEY_ITEM_RIGHT_HAND = "itemRightHand";
 	static final String KEY_DRIPLEAF_RIGHT_HAND = "dripleaf_right_hand";
 
-	private static final Identifier MODEL_RESOURCE = UMULittleMaid.identifier("geo/little_maid.geo.json");
-	private static final Identifier MODEL_DRIPLEAF_RESOURCE = UMULittleMaid.identifier("geo/little_maid_dripleaf.geo.json");
-	private static final Identifier TEXTURE_RESOURCE = UMULittleMaid.identifier("textures/entity/little_maid/little_maid.png");
-	private static final Identifier ANIMATION_RESOURCE = UMULittleMaid.identifier("animations/little_maid.animation.json");
-	private static final Identifier ANIMATION_TRANSFORM_RESOURCE = UMULittleMaid.identifier("animations/little_maid_transform.animation.json");
+	private static final Identifier RESOURCE_MODEL = UMULittleMaid.identifier("geo/little_maid.geo.json");
+	private static final Identifier RESOURCE_TEXTURE = UMULittleMaid.identifier("textures/entity/little_maid/little_maid.png");
+	private static final Identifier RESOURCE_TEXTURE_FENCER = UMULittleMaid.identifier("textures/entity/little_maid/little_maid_fencer.png");
+	private static final Identifier RESOURCE_TEXTURE_ARCHER = UMULittleMaid.identifier("textures/entity/little_maid/little_maid_archer.png");
+	private static final Identifier RESOURCE_TEXTURE_CRACKER = UMULittleMaid.identifier("textures/entity/little_maid/little_maid_cracker.png");
+	private static final Identifier RESOURCE_TEXTURE_GUARD = UMULittleMaid.identifier("textures/entity/little_maid/little_maid_guard.png");
+	private static final Identifier RESOURCE_TEXTURE_HEALER = UMULittleMaid.identifier("textures/entity/little_maid/little_maid_healer.png");
+	private static final Identifier RESOURCE_TEXTURE_POSEIDON = UMULittleMaid.identifier("textures/entity/little_maid/little_maid_poseidon.png");
+	private static final Identifier RESOURCE_TEXTURE_FARMER = UMULittleMaid.identifier("textures/entity/little_maid/little_maid_farmer.png");
+	private static final Identifier RESOURCE_TEXTURE_HUNTER = UMULittleMaid.identifier("textures/entity/little_maid/little_maid_hunter.png");
+	private static final Identifier RESOURCE_TEXTURE_KYUN = UMULittleMaid.identifier("textures/entity/little_maid/kyun/little_maid_kyun.png");
+	private static final Identifier RESOURCE_TEXTURE_FENCER_KYUN = UMULittleMaid.identifier("textures/entity/little_maid/kyun/little_maid_fencer_kyun.png");
+	private static final Identifier RESOURCE_TEXTURE_ARCHER_KYUN = UMULittleMaid.identifier("textures/entity/little_maid/kyun/little_maid_archer_kyun.png");
+	private static final Identifier RESOURCE_TEXTURE_CRACKER_KYUN = UMULittleMaid.identifier("textures/entity/little_maid/kyun/little_maid_cracker_kyun.png");
+	private static final Identifier RESOURCE_TEXTURE_GUARD_KYUN = UMULittleMaid.identifier("textures/entity/little_maid/kyun/little_maid_guard_kyun.png");
+	private static final Identifier RESOURCE_TEXTURE_HEALER_KYUN = UMULittleMaid.identifier("textures/entity/little_maid/kyun/little_maid_healer_kyun.png");
+	private static final Identifier RESOURCE_TEXTURE_POSEIDON_KYUN = UMULittleMaid.identifier("textures/entity/little_maid/kyun/little_maid_poseidon_kyun.png");
+	private static final Identifier RESOURCE_TEXTURE_FARMER_KYUN = UMULittleMaid.identifier("textures/entity/little_maid/kyun/little_maid_farmer_kyun.png");
+	private static final Identifier RESOURCE_TEXTURE_HUNTER_KYUN = UMULittleMaid.identifier("textures/entity/little_maid/kyun/little_maid_hunter_kyun.png");
+	private static final Identifier RESOURCE_ANIMATION = UMULittleMaid.identifier("animations/little_maid.animation.json");
+	private static final Identifier RESOURCE_ANIMATION_TRANSFORM = UMULittleMaid.identifier("animations/little_maid_transform.animation.json");
 
 	@Override
 	@SuppressWarnings("unchecked")
@@ -47,21 +65,40 @@ public class LittleMaidGeoModel extends AnimatedGeoModel<LittleMaidEntity> {
 
 	@Override
 	public Identifier getModelResource(LittleMaidEntity maid) {
-		// if (maid.isUsingDripleaf()) return MODEL_DRIPLEAF_RESOURCE;
-
-		return MODEL_RESOURCE;
+		return RESOURCE_MODEL;
 	}
 
 	@Override
 	public Identifier getTextureResource(LittleMaidEntity maid) {
-		// TODO: getTextureの処理をモデル側に移す
-		return maid.getTexture();
+		if (maid.isHeadpatted()) {
+			if      (maid.getJob().equals(ModEntities.JOB_FENCER))   return RESOURCE_TEXTURE_FENCER_KYUN;
+			else if (maid.getJob().equals(ModEntities.JOB_ARCHER))   return RESOURCE_TEXTURE_ARCHER_KYUN;
+			else if (maid.getJob().equals(ModEntities.JOB_CRACKER))  return RESOURCE_TEXTURE_CRACKER_KYUN;
+			else if (maid.getJob().equals(ModEntities.JOB_GUARD))    return RESOURCE_TEXTURE_GUARD_KYUN;
+			else if (maid.getJob().equals(ModEntities.JOB_HEALER))   return RESOURCE_TEXTURE_HEALER_KYUN;
+			else if (maid.getJob().equals(ModEntities.JOB_POSEIDON)) return RESOURCE_TEXTURE_POSEIDON_KYUN;
+			else if (maid.getJob().equals(ModEntities.JOB_FARMER))   return RESOURCE_TEXTURE_FARMER_KYUN;
+			else if (maid.getJob().equals(ModEntities.JOB_HUNTER))   return RESOURCE_TEXTURE_HUNTER_KYUN;
+			else                                                     return RESOURCE_TEXTURE_KYUN;
+		} else {
+			if      (maid.getJob().equals(ModEntities.JOB_FENCER))   return RESOURCE_TEXTURE_FENCER;
+			else if (maid.getJob().equals(ModEntities.JOB_ARCHER))   return RESOURCE_TEXTURE_ARCHER;
+			else if (maid.getJob().equals(ModEntities.JOB_CRACKER))  return RESOURCE_TEXTURE_CRACKER;
+			else if (maid.getJob().equals(ModEntities.JOB_GUARD))    return RESOURCE_TEXTURE_GUARD;
+			else if (maid.getJob().equals(ModEntities.JOB_HEALER))   return RESOURCE_TEXTURE_HEALER;
+			else if (maid.getJob().equals(ModEntities.JOB_POSEIDON)) return RESOURCE_TEXTURE_POSEIDON;
+			else if (maid.getJob().equals(ModEntities.JOB_FARMER))   return RESOURCE_TEXTURE_FARMER;
+			else if (maid.getJob().equals(ModEntities.JOB_HUNTER))   return RESOURCE_TEXTURE_HUNTER;
+			else                                                     return RESOURCE_TEXTURE;
+		}
 	}
 
 	@Override
-	public Identifier getAnimationResource(LittleMaidEntity animatable) {
-		if (animatable.isTransforming()) return ANIMATION_TRANSFORM_RESOURCE;
+	public Identifier getAnimationResource(LittleMaidEntity maid) {
+		// BlockBenchで保存すると、中途半端な値が揃えられてしまうため、これ以上の編集、保存をする必要がないようにファイル
+		// を分離します。つまり、これは完全に開発上の都合であり、仕組みとしては分ける意味はありません。
+		if (maid.isTransforming()) return RESOURCE_ANIMATION_TRANSFORM;
 
-		return ANIMATION_RESOURCE;
+		return RESOURCE_ANIMATION;
 	}
 }
